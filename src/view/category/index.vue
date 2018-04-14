4c46ad578ec37e863c001bbad9128d75d58cb166
Blackmack <hc_youxiang@sina.cn>
2018-04-14 20:43:58 +0800

    品类，商家接口对接

--- src/view/category/index.vue
+++ src/view/category/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="category-wrap">
+   <div class="searchCondition">
+           <el-form :inline="true"  class="demo-form-inline">
+             <el-form-item label="品类名称">
+               <el-input v-model="cagegoryName" placeholder="品类名称"></el-input>
+             </el-form-item>
+             <el-form-item>
+               <el-button type="primary" @click="search">查询</el-button>
+             </el-form-item>
+           </el-form>
+   </div>
     <div class="left-tree fl">
-      <el-tree :data="treeData" @node-click="nodeClick" :props="treeProps" show-checkbox default-expand-all node-key="id" ref="tree" highlight-current>
+      <el-tree :data="treeData" @node-click="nodeClick" :props="treeProps" show-checkbox default-expand-all node-key="id" ref="tree" highlight-current :expand-on-click-node="false">
+                 <span class="custom-tree-node" slot-scope="{ node, data }">
+                 <span>{{ node.label }}</span>
+                 <span><el-button type="text" size="mini" @click="() => deleteCategoryItem(node, data)">删除</el-button></span>
+                 </span>
       </el-tree>
     </div>
     <div class="right-forms">
@@ -33,20 +47,19 @@
   export  default {
     data() {
       return{
+        cagegoryName:'',
         categoryModel:{
           categoryDescribe: '',
-          categoryId: '2',
           categoryName: '',
-          creatorDate: '2018-04-12 12:12:12',
           creatorUser: 'huangc',
-          level: '',
+          level: null,
           parentId: null,
-          state: '1',
+          state: 1,     //1是有效，0无效
           updateUser: 'huangc'
         },
         treeProps:{
           label: 'categoryName',
-          children: 'categorList'
+          children: 'categoryList'
         },
         treeData:[]
       }
@@ -57,7 +70,10 @@
     methods: {
       ...mapActions(['getCategoryList','saveCategory','updateCategory','deleteCategory']),
       getTreeList(){
-          this.getCategoryList({}).then((data)=>{
+          let param={
+                  //cagegoryName:this.cagegoryName
+          }
+          this.getCategoryList(param).then((data)=>{
                this.treeData=data.category;
           })
       },
@@ -65,26 +81,73 @@
       onSubmit(categoryForm) {
         this.$refs[categoryForm].validate((valid) => {
             if(valid){
-              this.saveCategory(this.categoryModel).then((data)=>{
-                if(data.code==0){
-                  this.getTreeList();
-                  this.$message({
-                    type:'success',
-                    message:'保存成功！'
-                  });
-                }
-                else{
-                  this.$message({
-                    type:'success',
-                    message:data.msg
-                  });
-                }
-              })
+             if(this.categoryModel.categoryId){
+                  this.updateCategory(this.categoryModel).then((data)=>{
+                      if(data.code==0){
+                           this.getTreeList();
+                           this.$message({
+                             type:'success',
+                             message:'保存成功！'
+                           });
+                         }else{
+                           this.$message({
+                             type:'success',
+                             message:data.msg
+                           });
+                         }
+                       })
+             }
+             else{
+                this.saveCategory(this.categoryModel).then((data)=>{
+                    if(data.code==0){
+                      this.getTreeList();
+                      this.$message({
+                        type:'success',
+                        message:'保存成功！'
+                      });
+                    }
+                    else{
+                      this.$message({
+                        type:'success',
+                        message:data.msg
+                      });
+                    }
+                  })
+             }
             }
         })
       },
+      //查询
+      search() {
+         this.getTreeList();
+      },
+      //删除
+      deleteCategoryItem(node,data){
+           console.log(data.categoryId)
+          let param = {
+               categoryId:data.categoryId,
+               state:0
+          };
+          console.log(param)
+          this.deleteCategory(param).then((data)=>{
+                if(data.code==0){
+                    this.$message({
+                      message: '删除成功！',
+                      type: 'success'
+                    });
+                }
+                else{
+                    this.$message.error(data.msg);
+                }
+          })
+      },
       nodeClick(obj){
-         this.categoryModel.parentId=obj.id;
+         this.categoryModel.parentId=obj.parentId;
+         this.categoryModel.categoryId=obj.categoryId;
+         this.categoryModel.categoryDescribe=obj.categoryDescribe;
+         this.categoryModel.updateUser=obj.updateUser;
+         this.categoryModel.categoryName=obj.categoryName;
+         this.categoryModel.level=obj.level;
       }
     }
   }
@@ -116,5 +179,13 @@
     .el-form-item__error{
       top:0;
     }
+    .custom-tree-node {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 14px;
+        padding-right: 8px;
+      }
   }
 </style>
